bdaf3a3f72cea2739d18a709169166c2f2ab2845
hexis <hexis@void.lan>
2023-08-02 02:35:23 -0400

    added command to remove the hexfetch src after installing

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git clone https://github.com/hexisXz/hexfetch.git && mv hexfetch ~/ && cd hexfetch && gcc hexfetch.c -o hexfetch && sudo mv hexfetch /bin/ && sudo rm -r ~/hexfetch
+git clone https://github.com/hexisXz/hexfetch.git && mv hexfetch ~/Hexfetch && cd Hexfetch && gcc hexfetch.c -o hexfetch && sudo mv hexfetch /bin/ && sudo rm -r ~/hexfetch
