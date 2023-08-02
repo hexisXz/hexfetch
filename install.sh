5e2e9836c9e266b3c8feea7c6938109091a1dfa2
hexis <hexis@void.lan>
2023-08-02 02:41:06 -0400

    fixed install bug

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git clone https://github.com/hexisXz/hexfetch.git && mv hexfetch ~/hexfetch && cd hexfetch && gcc hexfetch.c -o hexfetch && sudo mv hexfetch /bin/
+git clone https://github.com/hexisXz/hexfetch.git && mv hexfetch ~/ && cd hexfetch && gcc hexfetch.c -o hexfetch && sudo mv hexfetch /bin/
